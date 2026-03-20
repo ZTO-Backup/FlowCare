493528505608b64cccbd80d991b9ee509744078c
ZTO-Backup <multimillionwealthcreation@gmail.com>
2026-03-20 19:20:06 +0000

    Converted app to PWA

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "flowcare-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/js/app.js"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
