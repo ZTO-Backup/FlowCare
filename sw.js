5effbd236b4a007f30f27083020d6b2e98883b93
ZTO-Backup <multimillionwealthcreation@gmail.com>
2026-03-22 18:40:10 +0000

    Fixed SW cache issue + stable version

--- sw.js
+++ sw.js
@@ -1,38 +1,17 @@
-const CACHE_NAME = "flowcare-v2";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./css/styles.css",
-  "./js/app.js"
-];
+const CACHE_NAME = "flowcare-v3";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.map(key => caches.delete(key)))
     )
   );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
